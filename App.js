a824c18c5ab8b22fc72813c2155c5b3a96ce9b3c
Gabriel1Vinicius2 <68132303+Gabriel1Vinicius2@users.noreply.github.com>
2020-07-10 21:09:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center'
     }
 })
